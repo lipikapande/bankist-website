10a89d4faeee16a7286d6b31d99ad10b26844f0a
Lipika <lipikapande@gmail.com>
2024-12-30 12:19:23 +0530

    third

--- script.js
+++ script.js
@@ -33,6 +33,12 @@ const allSections = document.querySelectorAll('.section');
 const allButtons = document.getElementsByTagName('button');
 const header = document.querySelector('.header');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('.#section-1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  window.scrollTo(s1coords.left, s1coords.top);
+});
 
 //creating and inserting elements
 
