96ab1db50e9a8359b017b07f3f49ffe7b65cdac8
Lipika <lipikapande@gmail.com>
2024-12-30 12:10:59 +0530

    second

--- script.js
+++ script.js
@@ -28,3 +28,29 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+const allSections = document.querySelectorAll('.section');
+const allButtons = document.getElementsByTagName('button');
+const header = document.querySelector('.header');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+
+//creating and inserting elements
+
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// message.innerHTML =
+//   'Lorem <button class = "btn btn--close-cookie">Got it!</button>';
+// header.prepend(message);
+// // header.append(message);
+// //prepend makes it the first child, append will make it the last.
+// //will be added only once.
+
+// header.append(message.cloneNode(true));
+// //message will appear twice now
+
+// header.before(message);
+// //message comes before header. .after will make it appear after the header.
+// //message.remove() will remove element.
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
+// //will transform primary color to orangered everywhere on the page.
