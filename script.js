e318469a7c65031b96a77316342f72c0fde4e9b6
Lipika <lipikapande@gmail.com>
2025-01-03 13:09:01 +0530

    image loading

--- script.js
+++ script.js
@@ -135,6 +135,27 @@ allSections.forEach(function (section) {
   section.classList.add('section--hidden');
 });
 
+//lazy loading images
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+//selects img elements with data-src attribute
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target);
+};
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
 //creating and inserting elements
 
 // const message = document.createElement('div');
