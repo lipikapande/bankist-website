53efd18d3a54b07801099b6a6350589397d34e34
Lipika <lipikapande@gmail.com>
2025-01-03 09:58:03 +0530

    reveal sections

--- script.js
+++ script.js
@@ -116,6 +116,25 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 
 headerObserver.observe(header);
 
+//reveal sections
+
+const revealSection = function (entries, observer) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+  });
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
 //creating and inserting elements
 
 // const message = document.createElement('div');
